310e308f1d9b91d206ecb429608df7b4a4674f1b
Christian Fei <crifei93@gmail.com>
2018-07-06 10:32:51 +0200

    mix format

--- lib/scraping_daringfireball_archive.ex
+++ lib/scraping_daringfireball_archive.ex
@@ -15,10 +15,11 @@ defmodule ScrapingDaringfireballArchive do
   end
 
   def scrape_archive_links(url \\ "https://daringfireball.net/archive/") do
-    IO.puts "started scraping for links #{url}"
+    IO.puts("started scraping for links #{url}")
+
     case HTTPoison.get(url) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        IO.puts "finished scraping for links #{url}"
+        IO.puts("finished scraping for links #{url}")
         {:ok, Parser.links(body)}
 
       {:error, %HTTPoison.Error{reason: reason}} ->
@@ -33,11 +34,12 @@ defmodule ScrapingDaringfireballArchive do
   end
 
   def scrape_number_of_words(%{href: url}) do
-    IO.puts "started scraping for words #{url}"
+    IO.puts("started scraping for words #{url}")
+
     case HTTPoison.get(url) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         words = Parser.words(body)
-        IO.puts "finished scraping for words #{url}"
+        IO.puts("finished scraping for words #{url}")
         {:ok, url, words}
 
       {:error, %HTTPoison.Error{reason: reason}} ->
